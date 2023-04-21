6010a047dbae8516f41aefa36dfd493109490638
Abigail <myemail@gmail.com>
2023-04-20 21:15:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry 
+ * main - Entry point
  *
- * Return: Always 0 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multil 
- lingual puzzle");
-    return (0);
+puts("\"Programming is like building a multil
+lingual puzzle");
+return (0);
 }
