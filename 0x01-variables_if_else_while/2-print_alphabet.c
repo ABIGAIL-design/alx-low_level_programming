0750c92bb03ea1fe99e9667b087dc2c1e99244bd
Abigail <myemail@gmail.com>
2023-04-21 15:31:01 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+	char letter = 'a';
+
+	while ( letter <= 'z')
+	{
+	    putchar ( letter);
+	    letter++;
+	    putchar("\n");
+	}
+	 return (0);
+}
