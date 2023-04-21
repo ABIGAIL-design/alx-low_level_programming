5f6d6dee4ff9f89aaa69270be66ba0abfaf41a64
Abigail <myemail@gmail.com>
2023-04-21 13:29:37 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - To detremine if the number is positive, negative or zero
  *
- * Return Always 0(Success)
+ * Return Always 0 (Success)
  */
 
 /* betty style doc for function main goes there */
