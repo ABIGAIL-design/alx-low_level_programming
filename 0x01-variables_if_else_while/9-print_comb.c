db7d781defb3b4bce160581d8d12ef882ba77a6e
Abigail <myemail@gmail.com>
2023-04-21 18:41:55 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,18 +5,18 @@
  */
 int main(void)
 {
-        int n;
+int n;
 
-        for (n = 48; n <= 57; n++)
-        {
-                putchar(n);
-                if (n == 57)
-                {
-                        continue;
-                }
-                putchar(',');
-                putchar(' ');
-        }
-        putchar('\n');
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+if (n == 57)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
 return (0);
 }
