eb9f7aa7719bdc26f783ef49e30e179f47460b06
Abigail <myemail@gmail.com>
2023-04-25 10:44:47 -0700

    git commit -m "o-putchar.c"

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main Entry point
+ * description Write a program that prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
