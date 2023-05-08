b57d13ab1f54fb138f592fccf0545d9b29f23b1e
Abigail <myemail@gmail.com>
2023-05-08 16:28:46 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
- /**
- *   print_chessboard - a function that prints  chessboard given set 2D array
+/**
+*   print_chessboard - print chessboard given set 2D array
 *    @a: 2D array
 */
+
 
 void print_chessboard(char (*a)[8])
 {
